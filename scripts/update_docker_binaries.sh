21d4b67087f4ab53db04ec7eb246316d4802e0b5
timor.kalerman <timor.kalerman@edgify.ai>
2023-10-18 14:04:56 +0300

    fix

--- scripts/update_docker_binaries.sh
+++ scripts/update_docker_binaries.sh
@@ -3,6 +3,7 @@ systemctl stop containerd
 wget https://download.docker.com/linux/static/stable/x86_64/docker-24.0.6.tgz
 tar xzvf docker-24.0.6.tgz
 cp docker/* /usr/bin/
+cp docker/* /usr/sbin/
 wget https://raw.githubusercontent.com/containerd/containerd/main/containerd.service -O /usr/lib/systemd/system/containerd.service
 sed -i 's/local\///g' /usr/lib/systemd/system/containerd.service
 wget https://raw.githubusercontent.com/moby/moby/master/contrib/init/systemd/docker.service -O /usr/lib/systemd/system/docker.service
